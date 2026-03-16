df9d8131c899a81f972f2c4290c9d31672beab1b
bbdjk <bbdjk06022@gmail.com>
2026-03-16 22:58:20 +0900

    Upgrade service worker to version 2 and manage caches
    Updated service worker to version 2 and added cache management for old versions.

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'game-tracker-v1.1';
+// 버전을 v2, v3 식으로 코드를 수정할 때마다 올려주시면 폰이 새 버전임을 인식합니다.
+const CACHE_NAME = 'game-tracker-v2'; 
 const FILES_TO_CACHE = [
   './',
   './index.html',
@@ -12,6 +13,21 @@ self.addEventListener('install', (evt) => {
     })
   );
   self.skipWaiting();
+});
+
+// 새로 추가된 부분: 옛날 버전(v1)의 기억을 지우고 새 버전(v2)으로 교체하는 역할
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('오래된 캐시 삭제 중...', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
